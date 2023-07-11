2e1a05420fffae987038ebe3fccca483e37b37fc
Fabian Müller <fmueller@owncloud.com>
2023-07-11 12:39:18 +0200

    Fix array syntax

--- install-extensions.sh
+++ install-extensions.sh
@@ -107,7 +107,7 @@ done
 
 if [[ "$(id -u)" == 0 ]]; then
     # we need some "dummy" command, calling a subshell works reasonably well
-    sudo_command=("sh", "-c")
+    sudo_command=("sh" "-c")
 fi
 
 if [[ -z "$sudo_command" ]]; then
